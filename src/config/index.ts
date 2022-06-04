232e51f62f8576519c68223121e9c967f24fbac6
windowdotonload <275981471@qq.com>
2022-06-04 21:02:21 +0800

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,21 @@
+const env = import.meta.env.MODE || "prod";
+const EnvConfig = {
+  dev: {
+    baseApi: "",
+    mockApi: "https://www.fastmock.site/mock/e6fa0eabe0aba670d319a7d4cec85120/api",
+  },
+  test: {
+    baseApi: "",
+    mockApi: "https://www.fastmock.site/mock/e6fa0eabe0aba670d319a7d4cec85120/api",
+  },
+  prod: {
+    baseApi: "",
+    mockApi: "https://www.fastmock.site/mock/e6fa0eabe0aba670d319a7d4cec85120/api",
+  },
+};
+
+export default {
+  env,
+  mock: true,
+  ...EnvConfig,
+};
