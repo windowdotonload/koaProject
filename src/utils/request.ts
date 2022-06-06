89fc518141c7b2433b5c09886189bb8271f29a84
windowdotonload <275981471@qq.com>
2022-06-06 23:52:53 +0800

    request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,6 +2,25 @@
 import config from "@/config/index";
 import axios from "axios";
 
+interface RequestParams {
+  method: "get" | "post" | "put" | "delete" | "patch";
+  url: string;
+  params: Record<string, any>;
+}
+
 const service = axios.create({
   baseURL: config.baseApi,
+  timeout: 8000,
 });
+
+service.interceptors.request.use((config) => {
+  return config;
+});
+
+service.interceptors.response.use((res) => {
+  return res;
+});
+
+export default function request(params: RequestParams) {
+  return service(params);
+}
