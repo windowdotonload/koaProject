61e2c9293ee7018fa8f4ad63bc3d5aec0330b5b6
windowdotonload <275981471@qq.com>
2022-06-08 23:19:35 +0800

    mod

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import config from "./config";
-import "./utils/request";
 import request from "./utils/request";
 
 const app = createApp(App);
