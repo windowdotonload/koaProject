6156174be974c2777bc382cf1f388255dbc2f666
windowdotonload <275981471@qq.com>
2022-06-15 22:16:57 +0800

    mod

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import config from "./config";
 import request from "./utils/request";
+import storage from "./utils/storage";
 
 const app = createApp(App);
+app.config.globalProperties.$request = request;
+app.config.globalProperties.$storage = storage;
 app.use(ElementPlus);
 app.mount("#app");
