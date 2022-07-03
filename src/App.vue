d23287251ed86027237270a07292e6bb2e9b5a12
windowdotonload <275981471@qq.com>
2022-07-03 23:55:40 +0800

    collapse

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <router-view></router-view>
@@ -13,10 +9,10 @@ import HelloWorld from "./components/HelloWorld.vue";
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
   overflow: hidden;
 }
 </style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,64 @@
 <template>
-  <div class="sider-container-left"></div>
+  <div class="sider-container-left">
+    <div class="switch-btn" @click="changeLeftWidth">
+      <span class="switch-flag"> {{ leftWidth == "5%" ? "》" : "《" }}</span>
+    </div>
+  </div>
   <div class="content-container-right"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const leftWidth = ref("15%");
 
-<style scoped>
+const changeLeftWidth = () => {
+  leftWidth.value = leftWidth.value === "5%" ? "15%" : "5%";
+};
+</script>
+
+<style scoped lang="scss">
 .sider-container-left {
+  transition: all 0.5s ease;
+  position: relative;
+  float: left;
   height: 100%;
-  width: 38.2%;
+  width: v-bind(leftWidth);
   background: yellow;
+}
+.content-container-right {
+  width: 100%;
+  padding-left: v-bind(leftWidth);
+  background-color: beige;
+}
+.sider-container-left:hover .switch-btn {
+  cursor: pointer;
+  display: flex;
+  right: -18rem;
+  opacity: 1;
+}
+
+.sider-container-left:hover .switch-flag {
+  opacity: 1;
+}
+.switch-btn {
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 16rem;
+  font-weight: bolder;
+  padding-right: 3%;
+  transition: all 0.5s ease;
+  width: 50rem;
+  height: 55rem;
+  background-color: yellow;
+  position: absolute;
+  top: 5%;
+  border-radius: 80%;
+  right: 0px;
+}
+.switch-flag {
+  transition: all 0.1s linear;
+  opacity: 0;
 }
 </style>
